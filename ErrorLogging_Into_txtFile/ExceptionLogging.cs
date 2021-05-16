ca039020c8868fda580470289f4ce7b369643f98
Jagpreet Singh Thakur <js1081996@gmail.com>
2021-05-16 23:05:33 +0530

    Uploadfile

---
+++ ErrorLogging_Into_txtFile/ExceptionLogging.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using context = System.Web.HttpContext;
+using System.Text;
+
+namespace ErrorLogg
+{
+    public class ExceptionLogging
+    {
+        private static String ErrorlineNo, Errormsg, extype, exurl, hostIp, ErrorLocation, HostAdd;
+
+        public static void SendErrorToText(Exception ex)
+        {
+            var line = Environment.NewLine + Environment.NewLine;
+
+            ErrorlineNo = ex.StackTrace.Substring(ex.StackTrace.Length - 7, 7);
+            Errormsg = ex.GetType().Name.ToString();
+            extype = ex.GetType().ToString();
+            exurl = context.Current.Request.Url.ToString();
+            ErrorLocation = ex.Message.ToString();
+
+            try
+            {
+                string filepath = context.Current.Server.MapPath("~/C:\\Users\\js108\\Documents\\Meddiff\\Errorlog.txt/");  
+
+                if (!Directory.Exists(filepath))
+                {
+                    Directory.CreateDirectory(filepath);
+
+                }
+                filepath = filepath + DateTime.Today.ToString("dd-MM-yy") + ".txt";   
+                if (!File.Exists(filepath))
+                {
+
+
+                    File.Create(filepath).Dispose();
+
+                }
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    string error = "Log Write  Date:" + " " + DateTime.Now.ToString() + line + "Error Line No :" + " " + ErrorlineNo + line + "Error Message:" + " " + Errormsg + line + "Exception Type:" + " " + extype + line + "Error Location :" + " " + ErrorLocation + line + " Error Page Url:" + " " + exurl + line + "User Host IP:" + " " + hostIp + line;
+                    sw.WriteLine("-----------Exception Details:  " + " " + DateTime.Now.ToString() + "-----------------");
+                    sw.WriteLine("-------------------------------------------------------------------------------------");
+                    sw.WriteLine(line);
+                    sw.WriteLine(error);
+                    sw.WriteLine("--------------------------------*Finish*------------------------------------------");
+                    sw.WriteLine(line);
+                    sw.Flush();
+                    sw.Close();
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+
+            }
+        }
+    } 
+}
